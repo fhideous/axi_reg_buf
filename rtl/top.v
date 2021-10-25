e4d0bc3870b0b3b2aca1de2be2f391b624f78206
unknown <root@a7211-4.sipc.miet.ru>
2021-10-25 17:20:02 +0300

    Fix(rtl/top.v) Полностью изм-л, опираясь на т. ист.
    Состояние определяется вектором значения ready_o и valid_o,
    каждое из которых определяется последовательно каждый в своем блоке,
    в зависимости от текущего состояния и входящих сигналов управления.
    Сигнал данных зависит от текущего состояния и возможных хэндшейков.
    Хэндшейк определяется комбинационно

--- rtl/top.v
+++ rtl/top.v
@@ -1,139 +1,72 @@
 
-module BUFFER( 
-    input               clk_i,
-    input               arstn_i,
+module BUFFER #(
+    parameter DATA_WIDTH = 3
+)(  
+    input                            clk_i,
+    input                            arstn_i,
 
-    input               tvalid_i,
-    output reg          tready_o,
-    input       [3:0]   tdata_i,
+    input                            tvalid_i,
+    output reg                       tready_o,
+    input       [DATA_WIDTH - 1:0]   tdata_i,
 
-    input               tready_i,
-    output reg          tvalid_o,
-    output reg  [3:0]   tdata_o
+    input                            tready_i,
+    output reg                       tvalid_o,
+    output reg  [DATA_WIDTH - 1:0]   tdata_o
 );
 
-reg   [3:0]             data_reg_0;
-reg   [3:0]             data_reg_1;
+reg  [DATA_WIDTH - 1:0]              data_hold;
 
-wire                    handshake_left;
-wire                    handshake_right;
+wire                                 handshake_left;
+wire                                 handshake_right;
 
-reg   [1:0]             state;  
-reg   [1:0]             next_state;
+wire [DATA_WIDTH - 1:0]              state;  
 
+assign state = {tready_o , tvalid_o};
 
-localparam    IDLE_S                 = 2'b00;
-localparam    ONE_BIT_HOLD_S         = 2'b01;  
-localparam    TWO_BIT_HOLD_S         = 2'b10;
+localparam    IDLE_S                 = 2'b10;
+localparam    ONE_BIT_HOLD_S         = 2'b11;  
+localparam    TWO_BIT_HOLD_S         = 2'b01;
+
+assign        handshake_left  = (state[1] && tvalid_i);
+assign        handshake_right = (state[0] && tready_i);
 
 always @( posedge clk_i ) begin
     if ( !arstn_i ) 
-        tready_o       <= 1;
+        tready_o      <= 1'b1;
     else begin
-        //if ( state == ONE_BIT_HOLD_S && tvalid_i && !tready_i)
-         if ( next_state == TWO_BIT_HOLD_S )
+        if (state == ONE_BIT_HOLD_S && tvalid_i && !tready_i)
             tready_o <= 1'b0;
-        //else if ( state == TWO_BIT_HOLD_S && tready_i )
-        else if ( state == TWO_BIT_HOLD_S && next_state == ONE_BIT_HOLD_S )
-            tready_o <= 1'b1; 
+        else if (state == TWO_BIT_HOLD_S && tready_i)
+            tready_o <= 1'b1;
     end
 end
 
 always @( posedge clk_i ) begin
     if ( !arstn_i )
-        tvalid_o      <= 0;
+        tvalid_o      <= 1'b0;
     else begin 
-        //if ( state == ONE_BIT_HOLD_S && !tvalid_i && tready_i )
-        if ( next_state == IDLE_S )
-            tvalid_o <= 1'b0;
-        //else if ( state == IDLE_S && tvalid_i)
-        else if ( next_state == ONE_BIT_HOLD_S )
+        if (state == IDLE_S && tvalid_i)
             tvalid_o <= 1'b1;
-
+        else if (state == ONE_BIT_HOLD_S && tready_i && !tvalid_i)
+            tvalid_o <= 1'b0;  
     end
 end
-
-//assign    handshake_left  = ( tready_o && tvalid_i ) ; /* || ( tvalid_i && handshake_right ) */ 
-//assign    handshake_right = ( tready_i && tvalid_o );
-assign    handshake_left  = (  tvalid_i ) ; /* || ( tvalid_i && handshake_right ) */ 
-assign    handshake_right = ( tready_i  );
 
 always @( posedge clk_i ) begin
     if ( !arstn_i ) begin
-        tdata_o     <= 
-        'b0;
-        data_reg_0  <= 'b0;
-        data_reg_1  <= 'b0;
+        tdata_o     <= 'b0;
+        data_hold   <= 'b0;
     end
     else begin
-        if      (state == IDLE_S         && handshake_left)
-            data_reg_0  <= tdata_i;
-        else if (state == TWO_BIT_HOLD_S && handshake_left && handshake_right) begin
-            tdata_o     <= data_reg_0;
-            data_reg_0  <= data_reg_1;
-            data_reg_1  <= tdata_i;
-        end
-        else if (state == ONE_BIT_HOLD_S && handshake_left && handshake_right) begin
-            tdata_o     <= data_reg_0;
-            data_reg_0  <= tdata_i;
-        end
+        if (state == IDLE_S && handshake_left)
+            tdata_o     <= tdata_i;
+        else if (state == ONE_BIT_HOLD_S && handshake_left && handshake_right)
+            tdata_o     <= tdata_i;
         else if (state == ONE_BIT_HOLD_S && handshake_left)
-            data_reg_1  <= tdata_i;
-        else if (state == ONE_BIT_HOLD_S && handshake_right)
-            tdata_o     <= data_reg_0;
-        else if (state == TWO_BIT_HOLD_S && handshake_right) begin
-            tdata_o     <= data_reg_0;
-            data_reg_0  <= data_reg_1;
-        end
+            data_hold   <= tdata_i;
+        else if (state == TWO_BIT_HOLD_S && handshake_right)
+            tdata_o     <= data_hold;
     end    
 end
 
-always @( posedge clk_i )
-  if( !arstn_i )
-    state <= IDLE_S;
-  else
-    state <= next_state;
-
-always @( * ) begin
-
-  case( state )
-    IDLE_S:
-      begin
-        if( handshake_left )
-          next_state = ONE_BIT_HOLD_S;
-        else 
-          next_state = IDLE_S;
-      end
-
-    ONE_BIT_HOLD_S:
-      begin
-        if ( handshake_left && handshake_right ) 
-          next_state  = ONE_BIT_HOLD_S;
-        else if( handshake_right ) 
-            next_state = IDLE_S;
-        else if ( handshake_left )
-            next_state = TWO_BIT_HOLD_S;
-        else 
-            next_state = ONE_BIT_HOLD_S;
-        end
-
-    TWO_BIT_HOLD_S:
-      begin
-        if( handshake_right && handshake_left )
-            next_state  = TWO_BIT_HOLD_S;
-        else if ( handshake_right )
-            next_state = ONE_BIT_HOLD_S;
-        else 
-            next_state = TWO_BIT_HOLD_S; 
-      end
-  
-    default:
-      begin
-        next_state = IDLE_S;
-      end
-
-  endcase
-end
-  
-  
   endmodule
