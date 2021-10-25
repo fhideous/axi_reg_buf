5b6da813a19f2efa4bab593203b1580420183afa
unknown <root@a7211-4.sipc.miet.ru>
2021-10-25 17:25:40 +0300

    Fix(tb) Проверил с существующим тестбенчом
    Тесты успешно проходятся.
    Если изменить модуль и защелкивать входные управляющие сигналы
    на триггере, то, хоть и допускаю, что работа моделя может быть
    некорректной, тестбенч ее не находт, т.к. не выдерживает валид до
    хэндшейка

--- tb/buffer_tb.v
+++ tb/buffer_tb.v
@@ -1,96 +1,126 @@
 `timescale 1ns / 1ps
 
-module buffer_tb;
-    reg         clk_i;
-    reg         arstn_i;
+module tb_ec_aa4_registred_axi();
 
-    reg         tvalid_i;
-    wire        tready_o;
-    reg  [3:0]  tdata_i;
+  parameter TOTAL_HS_QUAN           = 50000;
+  parameter CLK_I_SEMIPERIOD        = 3;
+  parameter PCQ_DELAY               = 1;
 
-    reg         tready_i;
-    wire        tvalid_o;
-    wire [3:0]  tdata_o;
+  parameter SS_DELAY_PARAM          = 3; // probability of delay insertion after handshake is equal to    1 / ( 1 + SS_DELAY_PARAM )
+  parameter MAX_SS_DELAY_IN_CYCLES  = 3; // set to 0 for test without ss delay
 
-    localparam CLK_SEMIPERIOD = 5;
+  parameter SM_DELAY_PARAM          = 3; // similar to SS_DELAY_PARAM
+  parameter MAX_SM_DELAY_IN_CYCLES  = 3; // similar to MAX_SS_DELAY_IN_CYCLES
 
-    BUFFER uut
-    (
-            .clk_i      (   clk_i       ),
-            .arstn_i    (   arstn_i     ),
-            
-            .tvalid_i   (   tvalid_i    ),
-            .tready_o   (   tready_o    ),
-            .tdata_i    (   tdata_i     ),
-            
-            .tready_i   (   tready_i    ),
-            .tvalid_o   (   tvalid_o    ),
-            .tdata_o    (   tdata_o     )
-    );
 
-    task buffer_test;
-        input reg           valid_i;
-        input reg           ready_i;
-        input reg  [3:0]    data_i;
-                
-        begin
-            tvalid_i    = valid_i;
-            tdata_i     = data_i;
-            tready_i    = ready_i;
-            #10
+  parameter DATA_WIDTH = 3;
 
-                $display("--------------");
-        end
 
-    endtask
+  reg                   clk_i;
+  reg                   aresetn_i;
+  reg                   ss_valid_i = 0;
+  reg [DATA_WIDTH-1:0]  ss_data_i  = 0;
+  wire                  ss_ready_o;
+  wire                  sm_valid_o;
+  wire [DATA_WIDTH-1:0] sm_data_o;
+  reg                   sm_ready_i = 0;
 
-    initial begin
-        clk_i = 'b0;
-        forever begin 
-              #CLK_SEMIPERIOD clk_i = ~clk_i;
-        end
+
+  BUFFER#(
+    .DATA_WIDTH    ( DATA_WIDTH  )
+  ) DUT (
+    .clk_i         ( clk_i       ),
+    .arstn_i       ( aresetn_i   ),
+
+    .tvalid_i      ( ss_valid_i  ),
+    .tdata_i       ( ss_data_i   ),
+    .tready_o      ( ss_ready_o  ),
+
+    .tvalid_o      ( sm_valid_o  ),
+    .tdata_o       ( sm_data_o   ),
+    .tready_i      ( sm_ready_i  )
+  );
+
+
+  initial begin
+    clk_i = 1'b1;
+    forever #CLK_I_SEMIPERIOD clk_i = !clk_i;
+  end
+
+
+  initial begin
+    aresetn_i = 0;
+
+    repeat( 1000 )
+      @( posedge clk_i );
+    #PCQ_DELAY;
+    aresetn_i <= 1; 
+  end
+
+
+  initial begin : ss_part
+    integer delay_quan;
+    integer i;
+    @( posedge clk_i );
+    while ( !aresetn_i )
+      @( posedge clk_i );
+    #PCQ_DELAY;
+
+    for ( i = 0; i < TOTAL_HS_QUAN; i = i + 1 ) begin
+      ss_valid_i <= 1;
+      ss_data_i  <= i % (2**DATA_WIDTH);
+
+      @( posedge clk_i );
+      while ( !ss_ready_o )
+        @( posedge clk_i );
+
+      #PCQ_DELAY;
+      ss_valid_i <= 0;
+
+      if ( !$urandom_range(SS_DELAY_PARAM, 0) && MAX_SS_DELAY_IN_CYCLES > 0 ) begin
+        delay_quan = $urandom_range(MAX_SS_DELAY_IN_CYCLES, 1);
+        repeat( delay_quan )
+          @( posedge clk_i );
+        #PCQ_DELAY;
+      end
+    end
+  end
+
+
+  initial begin : sm_part
+    integer delay_quan;
+    integer i;
+    @( posedge clk_i );
+    while ( !aresetn_i )
+      @( posedge clk_i );
+    #PCQ_DELAY;
+
+    for (  i = 0; i < TOTAL_HS_QUAN; i = i + 1 ) begin
+      sm_ready_i <= 1;
+
+      @( posedge clk_i );
+      while ( !sm_valid_o )
+        @( posedge clk_i );
+
+      if ( sm_data_o != (i % (2**DATA_WIDTH)) )begin
+        $display( "ERROR: sm_data_o = %0d, expected value = %0d at time %t", sm_data_o, (i % (2**DATA_WIDTH)), $time );
+        $stop;
+      end
+
+      #PCQ_DELAY;
+      sm_ready_i <= 0;
+
+      if ( !($random % (SM_DELAY_PARAM+1)) && MAX_SM_DELAY_IN_CYCLES > 0 ) begin
+        delay_quan = $urandom_range(MAX_SM_DELAY_IN_CYCLES, 1);
+        repeat( delay_quan )
+          @( posedge clk_i );
+        #PCQ_DELAY;
+      end
     end
 
-    initial begin
-        arstn_i         <=  0;
-        #400
-        arstn_i         <=  1;
-        
-        buffer_test(0, 0, 4'b0100);
-        buffer_test(1, 0, 4'b0001);
-        buffer_test(1, 0, 4'b1000);
-        buffer_test(1, 1, 4'b1000);
-        buffer_test(1, 1, 4'b0010);
-        buffer_test(0, 0, 4'b1100);
-        buffer_test(1, 1, 4'b1100);
-        buffer_test(1, 0, 4'b1101);
-        buffer_test(1, 1, 4'b1101);
-        buffer_test(0, 1, 4'b1001);
-        buffer_test(0, 1, 4'b1111);
-        #100
-        buffer_test(1, 1, 4'b0011);
-        buffer_test(1, 0, 4'b1001);
-        buffer_test(0, 0, 4'b1011);
-        buffer_test(0, 0, 4'b1011);
-        buffer_test(0, 1, 4'b1101);
-        buffer_test(1, 1, 4'b0111);
-        buffer_test(0, 1, 4'b1111);
-        
-        #100
-        buffer_test(1, 0, 4'b0011);
-        buffer_test(1, 1, 4'b1001);
-        buffer_test(1, 1, 4'b0011);
-        buffer_test(1, 1, 4'b1110);
-        buffer_test(1, 1, 4'b1000);
-        buffer_test(1, 1, 4'b1001);
-        buffer_test(1, 1, 4'b0011);
-        buffer_test(1, 1, 4'b1110);
-        buffer_test(1, 1, 4'b1000);
-        buffer_test(0, 1, 4'b1100);
-        buffer_test(0, 1, 4'b1110);
-        
+    $display( "INFO: Test successfully passed" );
+    $finish;
+  end
 
-
-    end
 
 endmodule
